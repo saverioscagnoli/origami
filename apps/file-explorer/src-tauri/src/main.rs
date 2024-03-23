77a08af97bdc611707709e3ebbe7f586ba5f639a
svscagn <95577971+saverioscagnoli@users.noreply.github.com>
2024-03-23 17:49:17 +0100

    see disk capacity

--- apps/file-explorer/src-tauri/src/main.rs
+++ apps/file-explorer/src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use serde::{ Deserialize, Serialize };
+use sysinfo::Disks;
 use tauri::Manager;
 
 #[tauri::command]
@@ -26,17 +28,31 @@ fn quit(app: tauri::AppHandle) {
   app.exit(0);
 }
 
+#[derive(Serialize, Deserialize)]
+struct Disk {
+  available_space: u64,
+  total_space: u64,
+}
+
+#[tauri::command]
+fn list_disks() -> Vec<Disk> {
+  let mut disks: Vec<Disk> = Vec::new();
+  for disk in Disks::new_with_refreshed_list().list() {
+    if disk.file_system() != "vfat" {
+      disks.push(Disk {
+        available_space: disk.available_space() / (1024 * 1024 * 1024),
+        total_space: disk.total_space() / (1024 * 1024 * 1024),
+      });
+    }
+  }
+
+  disks
+}
+
 fn main() {
   tauri::Builder
     ::default()
-    .invoke_handler(tauri::generate_handler![minimize, toggle_maximize, quit])
-    .setup(|app| {
-      let window = app.get_window("main").unwrap();
-
-      window.open_devtools();
-
-      Ok(())
-    })
+    .invoke_handler(tauri::generate_handler![minimize, toggle_maximize, quit, list_disks])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- apps/file-explorer/src/App.tsx
+++ apps/file-explorer/src/App.tsx
@@ -1,9 +1,14 @@
+import { Sidebar } from "@components/sidebar";
 import { Topbar } from "@components/topbar";
+import { cn } from "@utils";
 
 function App() {
   return (
     <div>
       <Topbar />
+      <div className={cn("w-screen h-[calc(100vh-2rem)]", "mt-8")}>
+        <Sidebar />
+      </div>
     </div>
   );
 }
